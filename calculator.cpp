999fdd9bc5d8a20d4bda9421ece698806d57dca3
minji <min572@snu.ac.kr>
2025-04-11 12:54:24 +0900

    changes

--- calculator.cpp
+++ calculator.cpp
@@ -3,16 +3,61 @@
 
 int Calculator::add(int a, int b) {
     // TODO
+    if (a<0 && b<0) {
+        if(a+b>0){
+        throw std::overflow_error("");}
+    }
+    else if (a>0 && b>0){
+        if(a+b<0){
+            throw std::overflow_error("");}
+        }
+    
+    return a+b;
 }
 
 int Calculator::sub(int a, int b) {
     // TODO
+    if (a<0 && b>0) {
+        if(a-b>0){
+        throw std::overflow_error("");}
+    }
+    else if (a>0 && b<0){
+        if(a-b<0){
+            throw std::overflow_error("");}
+    }
+    return a-b;
 }
 
 int Calculator::mul(int a, int b) {
     // TODO
+    if ((a<0 && b>0)||(a>0 && b<0) ){
+        if (a*b >0){
+            throw std::overflow_error("");
+        }
+    }
+    else if ((a<0 && b<0) || (a>0 && b>0)){
+        if (a*b<0){
+            throw std::overflow_error("");
+        }
+    }
+
+    return a*b;
 }
 
 int Calculator::div(int a, int b) {
     // TODO
+    if (b==0){
+        throw std::invalid_argument("");
+    }
+    else if ((a>0 && b<0) || (a<0 && b>0)){
+        if (a/b >0){
+            throw std::overflow_error("");
+        }
+    }
+    else if ((a<0 && b<0) || (a>0 && b>0)){
+        if (a/b<0){
+            throw std::overflow_error("");
+        }
+    }
+    return a/b;
 }
